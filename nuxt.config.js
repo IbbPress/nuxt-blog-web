60e2fc618b9eb76559b4ba62ad0a9117ee0b5b85
wencaizhang <1052642137@qq.com>
2020-03-15 21:51:08 +0800

    引入公用css

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.bootcss.com/normalize/8.0.1/normalize.min.css' }
     ]
   },
   /*
@@ -23,6 +24,8 @@ export default {
   ** Global CSS
   */
   css: [
+    '@/assets/css/main.css',
+    '@/assets/css/lib.css',
   ],
   /*
   ** Plugins to load before mounting the App
