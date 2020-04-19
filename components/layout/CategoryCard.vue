d3be6f0e48859e68c6e26224162250fe27462626
wencaizhang <1052642137@qq.com>
2020-04-19 20:34:30 +0800

    update styles

--- components/layout/CategoryCard.vue
+++ components/layout/CategoryCard.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="category-card">
-    <p>分类目录</p>
-    <p v-for="cate in category" :key="cate.id">
-      <nuxt-link :to="`/categories/${cate.name}`">
-        {{ cate.name }}
+    <h4 class="widget-title">
+      分类目录
+    </h4>
+    <ul>
+      <li v-for="cate in category" :key="cate.id" class="cat-item">
+        <nuxt-link :to="`/categories/${cate.name}`">
+          {{ cate.name }}
         <!-- ({{ cate.count }}) -->
-      </nuxt-link>
-    </p>
+        </nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -25,11 +29,37 @@ export default {
   background-color: #fff;
   margin-left: 1rem;
   margin-top: 1rem;
-  padding: 1rem;
+  padding: 1rem 1.5rem;
   max-height: 400px;
   overflow-y: auto;
-  p {
+  .widget-title {
+    position: relative;
+    margin-bottom: 1.5rem;
+    padding: 0;
+    &::after {
+      background-color: #1890ff;
+      content: "";
+      position: absolute;
+      width: 50px;
+      display: block;
+      height: 4px;
+      bottom: -10px;
+    }
+  }
+  ul {
+    padding: 0;
     margin: 0;
+  }
+  li {
+    list-style: none;
+  }
+  .cat-item {
+    margin: 0;
+    border-bottom: 1px solid #D9D9D9;
+    padding: 0.3em 0 .2em;;
+    &:last-child {
+      border-bottom: none;
+    }
   }
 }
 </style>
--- components/layout/TagsCard.vue
+++ components/layout/TagsCard.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="tags-card">
-    <p>标签</p>
-    <i-tag v-for="tag in tags" :key="tag.id">
-      <nuxt-link :to="`/tags/${tag.name}`">
-        {{ tag.name }}
-        <!-- ({{ tag.count }}) -->
-      </nuxt-link>
-    </i-tag>
+    <h4 class="widget-title">
+      标签
+    </h4>
+    <ul class="d-flex flex-wrap">
+      <li v-for="tag in tags" :key="tag.id" class="width: 33.33%;">
+        <i-tag>
+          <nuxt-link :to="`/tags/${tag.name}`">
+            {{ tag.name }}
+            <!-- ({{ tag.count }}) -->
+          </nuxt-link>
+        </i-tag>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -27,8 +33,29 @@ export default {
   background-color: #fff;
   margin-left: 1rem;
   margin-top: 1rem;
-  padding: 1rem;
+  padding: 1rem 1.5rem;
   max-height: 400px;
   overflow-y: auto;
+  .widget-title {
+    position: relative;
+    margin-bottom: 1.5rem;
+    padding: 0;
+    &::after {
+      background-color: #1890ff;
+      content: "";
+      position: absolute;
+      width: 50px;
+      display: block;
+      height: 4px;
+      bottom: -10px;
+    }
+  }
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+  li {
+    list-style: none;
+  }
 }
 </style>
